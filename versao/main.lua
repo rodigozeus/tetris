eb80c21bf700c0281aa056847502622dd611cc1f
rodigozeus <rodigozeus@hotmail.com>
2026-04-06 20:44:56 -0300

    Increase font size

--- versao/main.lua
+++ versao/main.lua
@@ -1,5 +1,12 @@
+local font
+
+function love.load()
+  font = love.graphics.newFont(60)
+  love.graphics.setFont(font)
+end
+
 function love.draw()
-  love.graphics.printf("versão 2", 0, 220, 640, "center")
+  love.graphics.printf("versão 1", 0, 200, 640, "center")
 end
 
 function love.keypressed()
