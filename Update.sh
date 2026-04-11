1e83d0f662baa29d3f9ddf1d7cef1dcb0b541567
rodigozeus <rodigozeus@hotmail.com>
2026-04-11 07:46:49 -0300

    compatibilidade entre consoles

--- Update.sh
+++ Update.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+MODEL=$(cat /proc/device-tree/model 2>/dev/null | tr -d '\0')
+
+if echo "$MODEL" | grep -qi "rg-ds"; then
+  swaymsg 'output DSI-1 power on'
+fi
+
 LD_LIBRARY_PATH=/storage/roms/ports/moonlightnew/libs \
   /storage/roms/ports/moonlightnew/love \
   /storage/roms/ports/update
+
+if echo "$MODEL" | grep -qi "rg-ds"; then
+  swaymsg 'output DSI-1 power off'
+fi
