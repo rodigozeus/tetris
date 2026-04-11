765905ebcc58887359ad64d2dbcd8acf1ddcb157
rodigozeus <rodigozeus@hotmail.com>
2026-04-11 17:43:54 -0300

    picross removido

--- picross/conf.lua
+++
@@ -1,9 +0,0 @@
-function love.conf(t)
-  t.window.title     = "Picross"
-  t.window.width     = 1280
-  t.window.height    = 480
-  t.window.fullscreen = false
-  t.window.borderless = true
-  t.window.x         = 0
-  t.window.y         = 0
-end
