96eb7a154f4f5dcdef143b24c5bac1d1258d03bb
rodigozeus <rodigozeus@hotmail.com>
2026-04-21 14:09:35 -0300

    correção do mic do zelda

--- Zelda_PH.sh
+++ Zelda_PH.sh
@@ -5,6 +5,7 @@ sed -i 's/^screen_swap = .*/screen_swap = 1/' "$CFG"
 sed -i 's/^mirror_touch = .*/mirror_touch = 1/' "$CFG"
 
 pactl set-default-source alsa_input._sys_devices_platform_sound_sound_card0.HiFi__Mic__source
+export SDL_AUDIODRIVER=pulse
 
 /usr/bin/start_drastic.sh "/storage/roms/nds/Zelda_PH_PTBR_Dpad_Final.nds"
 
